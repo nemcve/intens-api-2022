45b45197940aa3187dd903a4f2ae2a68941af485
Nemanja Cvejic <37965912+nemcve@users.noreply.github.com>
2023-04-18 21:39:21 +0200

    Update DemoController.java

--- src/main/java/intens/api/praksa2022/controller/DemoController.java
+++ src/main/java/intens/api/praksa2022/controller/DemoController.java
@@ -9,6 +9,6 @@ public class DemoController {
     @RequestMapping("/")
     public ResponseEntity<String> getRootDemo() {
 
-        return ResponseEntity.ok().body("Intens Praksa 2023! Nemanja Cvejić");
+        return ResponseEntity.ok().body("Intens Praksa 2023!");
     }
 }
